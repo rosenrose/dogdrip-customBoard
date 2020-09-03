7530ef8c34ba2dae82393d60a959e3745e0f3696
rosenrose <42435748+rosenrose@users.noreply.github.com>
2020-09-04 05:29:36 +0900

    1.1 업데이트

--- options.js
+++ options.js
@@ -1,80 +1,138 @@
-var boardList = [];
-var userBoardList = [];
+let boardList = [];
+let userBoardList = [];
+let draggables = Array.from(document.querySelectorAll("#user td")).slice(2);
+let dragged;
 
-userSelects = document.querySelector("table~table").querySelectorAll("select");
 fetch("https://www.dogdrip.net/")
 .then(response => response.text())
 .then(content => {
     content = new DOMParser().parseFromString(content,"text/html");
-    dogdripMain = content.querySelectorAll("div.eq.section.secontent.background-color-content > div.xe-widget-wrapper");
-    top_boards = dogdripMain[1].querySelectorAll("div > div.xe-widget-wrapper");
-    bottom_boards = dogdripMain[7].querySelectorAll("div > div.xe-widget-wrapper");
-    middle_boards = Array.prototype.slice.call(dogdripMain).slice(2,6);
+    boardList = content.querySelectorAll("div.eq.overflow-hidden");
+    boardList = Array.from(boardList).map(board => board.querySelector("a.eq.link").textContent.trim());
 
-    tableTds = document.querySelector("table").querySelectorAll("td");
-    for (let i=0; i<top_boards.length; i++) {
-        boardName = top_boards[i].querySelector("a.eq.link").text;
-        tableTds[i].innerText = boardName;
-        boardList.push(boardName);
-    }
-    for (let i=0; i<middle_boards.length; i++) {
-        boardName = middle_boards[i].querySelector("a.eq.link").text;
-        tableTds[i+top_boards.length].innerText = boardName;
-        boardList.push(boardName);
-    }
-    for (let i=0; i<bottom_boards.length; i++) {
-        boardName = bottom_boards[i].querySelector("a.eq.link").text;
-        tableTds[i+top_boards.length+middle_boards.length].innerText = boardName;
-        boardList.push(boardName);
+    for (let i=0; i<2; i++) {
+        document.querySelectorAll("#original td")[i].textContent = boardList[i];
     }
     boardList = boardList.slice(2);
 
-    for (let i=0; i<userSelects.length; i++) {
-        userSelects[i].id = i;
-        userSelects[i].addEventListener("change", event => {
-            userBoardList[event.target.id] = event.target.value;
-            chrome.storage.sync.set({userBoardList: userBoardList}, ()=>{});
-            window.location.reload();
+    let labels = document.querySelectorAll("label");
+    for (let i=0; i<labels.length; i++) {
+        labels[i].appendChild(document.createTextNode(boardList[i]));
+        let input = labels[i].querySelector("input");
+        input.value = boardList[i];
+        input.addEventListener("change", event => {
+            if (event.target.checked) {
+                userBoardList.push(event.target.value);
+            }
+            else {
+                let idx = userBoardList.indexOf(event.target.value);
+                if (idx > -1) userBoardList.splice(idx, 1);
+            }
+            save();
+            setTables();
         });
-        for (let j=0; j<boardList.length; j++) {
-            let option = document.createElement("option");
-            option.value = boardList[j];
-            option.text = boardList[j];            
-            userSelects[i].appendChild(option);
-        }
     }
+
     chrome.storage.sync.get('userBoardList', data => {
         if (data.userBoardList == undefined) {
-            for (let i=0; i<boardList.length; i++) {
-                userBoardList.push(boardList[i]);
-            }
-            chrome.storage.sync.set({userBoardList: userBoardList}, ()=>{});
+            userBoardList = boardList.slice();
+            save();
         }
         else {
-            userBoardList = data.userBoardList;
+            userBoardList = data.userBoardList.slice();
+            if (userBoardList.length > boardList.length) {
+                userBoardList.length = boardList.length;
+                save();
+            }
         }
-        userSelectUpdate();
+        setTables();
     });
 });
 
-function userSelectUpdate() {
-    for (let i=0; i<userSelects.length; i++) {
-        let options = userSelects[i].querySelectorAll("option");
-        for (let j=0; j<options.length; j++) {
-            if (options[j].value == userBoardList[i]) {
-                options[j].selected = true;
-                tableTds[j+2].style.backgroundColor = "rgb(0,147,219)";
+function setTables() {
+    let checkboxes = document.querySelectorAll("input[type='checkbox']");
+    for (let checkbox of checkboxes) {
+        checkbox.checked = userBoardList.includes(checkbox.value);
+    }
+    for (let i=0; i<draggables.length; i++) {
+        if (i < userBoardList.length) {
+            draggables[i].textContent = userBoardList[i];
+            draggables[i].style.backgroundColor = "rgb(46,67,97)";
+            draggables[i].draggable = true;
+            if (!draggables[i].hasAttribute("event")) {
+                draggables[i].id = i;
+                draggables[i].setAttribute("event", true);
+                draggables[i].addEventListener("dragstart", event => {
+                    if (event.target.draggable) {
+                        dragged = event.target;
+                        event.target.style.opacity = 0.5;
+                    }
+                });
+                draggables[i].addEventListener("dragend", event => {
+                    event.target.style.opacity = 1;
+                    dragged = null;
+                });
+                draggables[i].addEventListener("dragover", event => {
+                    event.preventDefault();
+                });
+                draggables[i].addEventListener("dragenter", event => {
+                    if (dragged && event.target.draggable) {
+                        event.target.style.backgroundColor = "rgb(56,138,255)";
+                    }
+                });
+                draggables[i].addEventListener("dragleave", event => {
+                    if (dragged && event.target.draggable) {
+                        event.target.style.backgroundColor = "rgb(46,67,97)";
+                    }
+                });
+                draggables[i].addEventListener("drop", event => {
+                    event.preventDefault();
+                    if (dragged && event.target.draggable) {
+                        event.target.style.backgroundColor = "rgb(46,67,97)";
+                        if (dragged != event.target) {
+                            [event.target.textContent, dragged.textContent] = [dragged.textContent, event.target.textContent];
+                            userBoardList[dragged.id] = dragged.textContent;
+                            userBoardList[event.target.id] = event.target.textContent;
+                            save();
+                        }
+                    }
+                });
             }
+        }
+        else {
+            draggables[i].textContent = "\u00A0";
+            draggables[i].style.backgroundColor = "";
+            draggables[i].draggable = false;
         }
     }
 }
 
-document.querySelector("#resetButton").addEventListener('click', () => {
-    for (let i=0; i<userSelects.length; i++) {
-        let options = userSelects[i].querySelectorAll("option");
-        options[i].selected = true;
-        userBoardList[i] = boardList[i];
-    }
-    chrome.storage.sync.set({userBoardList: userBoardList}, ()=>{});
+document.querySelector("#reset").addEventListener("click", () => {
+    chrome.storage.sync.set({userBoardList: boardList}, ()=>{});
     window.location.reload();
 });
+
+document.querySelector("#all").addEventListener("click", () => {
+    for (let input of document.querySelectorAll("input[type='checkbox']")) {
+        if (!input.checked) {
+            input.checked = true;
+            userBoardList.push(input.value);
+        }
+    }
+    save();
+    setTables();
+});
+
+document.querySelector("#none").addEventListener("click", () => {
+    for (let input of document.querySelectorAll("input[type='checkbox']")) {
+        input.checked = false;
+    }
+    userBoardList = [];
+    save();
+    setTables();
+});
+
+function save() {
+    chrome.storage.sync.set({userBoardList: userBoardList}, ()=>{});
+    
+}
--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-let main = document.querySelectorAll("div.eq.section.secontent.background-color-content > div.xe-widget-wrapper");
-let top_boards = main[1].querySelectorAll("div > div.xe-widget-wrapper");
-let bottom_boards = main[7].querySelectorAll("div > div.xe-widget-wrapper");
-let middle_boards = Array.prototype.slice.call(main).slice(2,6);
-main[6].style.display = "none";
-let boards = {};
-
-boards[top_boards[2].querySelector("a.eq.link").innerText] = top_boards[2].innerHTML;
-for (let i=0; i<middle_boards.length; i++) {
-    boards[middle_boards[i].querySelector("a.eq.link").innerText] = middle_boards[i].innerHTML;
-}
-for (let i=0; i<bottom_boards.length; i++) {
-    boards[bottom_boards[i].querySelector("a.eq.link").innerText] = bottom_boards[i].innerHTML;
+let boardList = document.querySelectorAll("div.eq.overflow-hidden");
+boardList = Array.from(boardList).slice(2);
+for (let board of boardList) {
+    boardList[board.querySelector("a.eq.link").textContent.trim()] = board.innerHTML;
 }
 
 chrome.storage.sync.get('userBoardList', data => {
-    top_boards[2].innerHTML = boards[data.userBoardList[0]];
-    for (let i=0; i<middle_boards.length; i++) {
-        middle_boards[i].innerHTML = boards[data.userBoardList[i+1]];
-    }
-    for (let i=0; i<bottom_boards.length; i++) {
-        bottom_boards[i].innerHTML = boards[data.userBoardList[i+1+middle_boards.length]];
+    for (let i=0; i<boardList.length; i++) {
+        if (i < data.userBoardList.length) {
+            boardList[i].innerHTML = boardList[data.userBoardList[i]];
+        }
+        else {
+            boardList[i].style.display = "none";
+        }
     }
 });
